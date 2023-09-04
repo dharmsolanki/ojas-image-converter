c1b7a849df5e89b6f7b1ddcf7d88b663048a892d
Dharm Solanki <dharmsolanki1010@gmail.com>
2023-09-04 19:52:48 +0530

    chnages

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,94 +1,94 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import "../css/Form.css";
 
 export default function Form() {
-
-  const [name, setName] = useState("");
   const [image, setImage] = useState("");
-
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  }
+  const [signature, setSignature] = useState("");
+  const [isGrayscale, setIsGrayscale] = useState(false);
 
   const handleImageChange = (event) => {
     if (event.target && event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0];
-  
-      // Use FileReader to read the selected file as a data URL
+
       const reader = new FileReader();
-  
+
       reader.onload = (e) => {
-        // The result contains the data URL representing the selected image
         const dataURL = e.target.result;
         setImage(dataURL);
       };
-  
-      reader.readAsDataURL(file); // Read the file as a data URL
+      reader.readAsDataURL(file);
     }
   };
-  
+
+  const handleSignChange = (event) => {
+    if (event.target && event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+
+      setIsGrayscale(!isGrayscale);
+
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const dataURL = e.target.result;
+        setSignature(dataURL);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <>
       <div className="container mt-5">
-      <div class="border border-info border-2 p-4 mb-4">
-        <div className="row">
-          <div className="col-sm">
-            <div className="form-group">
-              <label htmlFor="exampleInputName">Name of Birthday Person</label>
-              <input
-                type="text"
-                className="form-control mt-2"
-                id="exampleInputName"
-                placeholder="Name of Birthday Person"
-                value={name}
-                onChange={handleNameChange}
-              />
+        <div className="border border-info border-2 p-4 mb-4">
+          <div className="row">
+            <div className="col-sm">
+              <div className="form-group">
+                <label htmlFor="exampleInputName">Upload Photograph</label>
+                <input
+                  type="file"
+                  className="form-control mt-2"
+                  id="exampleInputImage"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="row mt-4">
+            <div className="col">
+              <div className="form-group">
+                <label htmlFor="exampleInputSign">Upload Signature</label>
+                <input
+                  type="file"
+                  className="form-control mt-2"
+                  id="exampleInputSign"
+                  accept="image/*"
+                  onChange={handleSignChange}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="row mt-4">
+            <div className="col">
+              <div className="form-group">
+                <button className="btn btn-success" style={{ width: "100%" }}>Convert</button>
+              </div>
             </div>
           </div>
         </div>
-
-        <div className="row mt-4">
-          <div className="col">
-            <div className="form-group">
-              <label htmlFor="exampleInputName">Upload Image</label>
-              <input
-                type="file"
-                className="form-control mt-2"
-                id="exampleInputImage"
-                accept="image/*" // Specify that only image files are allowed
-                onChange={handleImageChange}
-              />
-            </div>
-          </div>
-        </div>
-
-        {/* <div className="row mt-4">
-          <div className="col">
-            <div className="form-group">
-              <label htmlFor="exampleInputDesc">Description</label>
-              <textarea className="form-control mt-2" id="exampleTextarea" rows="2" placeholder="Enter Your Birthday Description"></textarea>
-            </div>
-          </div>
-        </div> */}
-
-        <div className="row mt-4">
-          <div className="col">
-            <div className="form-group">
-              <button className="btn btn-success" style={{ width: "100%" }}>Create</button>
-            </div>
-          </div>
-        </div>
-      </div>
       </div>
 
       <div className="poster">
-       
-       {image && (
+        {image && (
+          <img src={image} alt="" className="uploaded-img" />
+        )}
 
-         <img src={image} alt="" height="300px" width="290px" className="uploaded-img" />
-       )
-
-       }
+        {signature && (
+          <img src={signature} alt="" className={isGrayscale ? "grayscale" : ""} />
+        )}
       </div>
     </>
   );
